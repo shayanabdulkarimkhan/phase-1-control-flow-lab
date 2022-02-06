77653ed822d0a4ab88e586511f4166f89ba51545
shayan <sak704@nyu.edu>
2022-02-06 15:05:07 -0800

    Completed Lab

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
-}
+  if (distance <= 400){
+    return `This one is on me!`;
+  } else if (distance > 2500) {
+    return `No can do.`;
+    
+  } else if (distance > 2000) {
+    return `I will gladly take your thirty bucks.`;
+  };
+};
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
-}
+  let approval = "Ok, sounds good.";
+  let rejection = "No go."
 
-function switchOnCharmFromTip(){
+  const response = (city === 'NYC') ? approval : rejection;
+
+  return response;
+};
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+
+  switch (tip) {
+    case `generous`:
+      return `Thank you so much.`;
+      break;
+    
+    case `not as generous`:
+      return `Thank you.`;
+      break;
+  
+    default:
+      return `Bye.`;
+      break;
+  }
 }
